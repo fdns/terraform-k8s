d07775c8eb1102f1bd0ccd17adb1841f417d28c4
Felipe Espinoza <yayofelipe@gmail.com>
2020-02-10 10:40:14 -0300

    Adding wildcard domain

--- modules/instances/init.sh
+++ modules/instances/init.sh
@@ -6,3 +6,4 @@ sudo -u ec2-user echo "${key}" >> /home/ec2-user/.ssh/authorized_keys
 %{ endfor ~}
 export NAMECHEAP=$(aws --region=us-east-2 ssm get-parameter --name "namecheap" --query Parameter.Value --output text)
 curl "https://dynamicdns.park-your-domain.com/update?host=k8s&domain=espinoza.dev&password=$NAMECHEAP"
+curl "https://dynamicdns.park-your-domain.com/update?host=*.k8s&domain=espinoza.dev&password=$NAMECHEAP"
